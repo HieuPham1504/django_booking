09c8d5c4113c86e4abd42ee597c297d9401b859d
Phạm Trọng Hiếu <phamtronghieu686@gmail.com>
2023-12-07 16:22:14 +0700

    update css

--- ms_booking/settings.py
+++ ms_booking/settings.py
@@ -104,10 +104,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_db',
-        'USER': 'hieupt_03',
+        'USER': 'hieupt',
         'PASSWORD': '0176',
-        'HOST': '10.0.254.131',
-        'PORT': '5432',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
@@ -131,29 +131,29 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Logging
 #
-LOGGING = {
-    'version': 1,
-    # The version number of our log
-    'disable_existing_loggers': False,
-    # django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
-    # A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
-    'handlers': {
-        'file': {
-            'level': 'WARNING',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join('//logging/django_booking.log'),
-        },
-    },
-    # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
-    'loggers': {
-       # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
-        '': {
-            'handlers': ['file'], #notice how file variable is called in handler which has been defined above
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     # The version number of our log
+#     'disable_existing_loggers': False,
+#     # django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
+#     # A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
+#     'handlers': {
+#         'file': {
+#             'level': 'WARNING',
+#             'class': 'logging.FileHandler',
+#             'filename': os.path.join('//logging/django_booking.log'),
+#         },
+#     },
+#     # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
+#     'loggers': {
+#        # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
+#         '': {
+#             'handlers': ['file'], #notice how file variable is called in handler which has been defined above
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
