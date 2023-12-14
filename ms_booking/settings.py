becf60a7a714dfaab215aab1f288d517fdeee6a9
Phạm Trọng Hiếu <phamtronghieu686@gmail.com>
2023-12-14 09:39:21 +0700

    setting

--- ms_booking/settings.py
+++ ms_booking/settings.py
@@ -190,5 +190,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'phamtronghieu686@gmail.com'
-EMAIL_HOST_PASSWORD = 'azpm oher btdc mayi'
+EMAIL_HOST_USER = 'mapstarcom@gmail.com'
+EMAIL_HOST_PASSWORD = 'kavz ebsg usgl evif'
