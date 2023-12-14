906d9e875b31b0a9c8af72292345c72388fe94c1
Phạm Trọng Hiếu <phamtronghieu686@gmail.com>
2023-12-14 16:49:46 +0700

    add background field

--- ms_destination/models.py
+++ ms_destination/models.py
@@ -4,7 +4,9 @@ from ms_company.models import MsCompany
 class MsDestination(models.Model):
   name = models.CharField(max_length=100, null=False)
   description = models.TextField(null=False)
-  background = models.ImageField(upload_to='images/')
+  background = models.ImageField(upload_to='destination/')
+  destination_background_page = models.ImageField(upload_to='destination/', null=True, blank=True)
+
   company = models.ForeignKey(MsCompany, on_delete=models.SET_NULL, null=True, blank=True)
   priority = models.IntegerField(default=1000)
 
