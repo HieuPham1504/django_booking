77d4022a3d2ff31fb0bb528e3335aefe021b0d1b
Phạm Trọng Hiếu <phamtronghieu686@gmail.com>
2023-12-25 16:56:08 +0700

    a

--- ms_property/admin.py
+++ ms_property/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import MsProperty
 
 class PropertyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "destination_id")
 
 
 admin.site.register(MsProperty, PropertyAdmin)
