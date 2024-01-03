aa47fdf85ebb1714ae344c928951a17281772752
HieuPT <hieupt@sapo.vn>
2024-01-03 15:20:30 +0700

    daterange booking

--- productionfiles/js/ms_booking_list.js
+++ productionfiles/js/ms_booking_list.js
@@ -1,4 +1,118 @@
 $(document).ready(function () {
+    $('#date-start.js_date_picker').daterangepicker({
+            opens: 'right',
+            language: 'vi',
+            autoUpdateInput: false,
+            startDate: new Date(),
+            minDate: new Date(),
+            function(start, end, label) {
+                debugger
+                $(this).val(start.format("DD/MM/YYYY"));
+            },
+            locale: {
+                "format":
+                    'DD/MM/YYYY',
+                "applyLabel":
+                    "Chọn",
+                "cancelLabel":
+                    "Hủy",
+                "daysOfWeek":
+                    [
+                        "CN",
+                        "T2",
+                        "T3",
+                        "T4",
+                        "T5",
+                        "T6",
+                        "T7"
+                    ],
+                "monthNames":
+                    [
+                        "Tháng 1",
+                        "Tháng 2",
+                        "Tháng 3",
+                        "Tháng 4",
+                        "Tháng 5",
+                        "Tháng 6",
+                        "Tháng 7",
+                        "Tháng 8",
+                        "Tháng 9",
+                        "Tháng 10",
+                        "Tháng 11",
+                        "Tháng 12"
+                    ],
+            }
+            ,
+        },
+
+        function (start, end, label) {
+            $('.js_date_picker#date-start').val(start.format('DD/MM/YYYY'))
+            $('.js_date_picker#date-end').val(end.format('DD/MM/YYYY'))
+        }
+
+        ,
+        $('#date-start.js_date_picker').on('apply.daterangepicker', function (ev, picker) {
+            $(this).val(picker.startDate.format('DD/MM/YYYY')); // set value for this
+            // anything else you want to do
+        })
+    );
+
+    $('#date-end.js_date_picker').daterangepicker({
+            opens: 'left',
+            language: 'vi',
+            autoUpdateInput: false,
+            startDate: new Date(),
+            minDate: new Date(),
+            function(start, end, label) {
+                debugger
+                $(this).val(start.format("DD/MM/YYYY"));
+            },
+            locale: {
+                "format":
+                    'DD/MM/YYYY',
+                "applyLabel":
+                    "Chọn",
+                "cancelLabel":
+                    "Hủy",
+                "daysOfWeek":
+                    [
+                        "CN",
+                        "T2",
+                        "T3",
+                        "T4",
+                        "T5",
+                        "T6",
+                        "T7"
+                    ],
+                "monthNames":
+                    [
+                        "Tháng 1",
+                        "Tháng 2",
+                        "Tháng 3",
+                        "Tháng 4",
+                        "Tháng 5",
+                        "Tháng 6",
+                        "Tháng 7",
+                        "Tháng 8",
+                        "Tháng 9",
+                        "Tháng 10",
+                        "Tháng 11",
+                        "Tháng 12"
+                    ],
+            }
+            ,
+        },
+
+        function (start, end, label) {
+            $('.js_date_picker#date-start').val(start.format('DD/MM/YYYY'))
+            $('.js_date_picker#date-end').val(end.format('DD/MM/YYYY'))
+        },
+        $('#date-end.js_date_picker').on('apply.daterangepicker', function (ev, picker) {
+            $(this).val(picker.endDate.format('DD/MM/YYYY')); // set value for this
+            // anything else you want to do
+        })
+    );
+
     $('.booking-detail-row').click(function (ev) {
         let currentRow = ev.currentTarget.dataset
         let bookingId = parseInt(currentRow.bookingId)
